6750c885e924dcbca0799bbec2daccea07652452
art1factia <akea1027th@gmail.com>
2024-11-10 12:34:27 +0900

    git ignore

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Canvas>
+      <Canvas style={{height: "700px"}}>
         <RotatingCube />
         <ambientLight intensity={0.1} />
         <directionalLight position={[0, 0, 5]} color="red" />
